39f7512c0265dd471d15fa3c9f2b488e074258c3
harooks <haruko.flipsy.krb@gmail.com>
2025-09-28 19:24:36 -0500

    feat: replace product page with farm page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", () => {
   const docHeight = document.documentElement.scrollHeight - window.innerHeight;
   const scrollPercent = scrollTop / docHeight;
 
-  const maxMove = window.innerHeight - 40; // 上下移動可能距離
+  const maxMove = window.innerHeight - 40;
   document.querySelector(".scroll-indicator").style.transform = `translateY(${
     scrollPercent * maxMove
   }px)`;
